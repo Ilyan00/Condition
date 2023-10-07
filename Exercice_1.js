fa5adecf4265c5fd5b22bbbe7bc6dc6aabe7dcd9
Ilyan <146921733+Ilyan00@users.noreply.github.com>
2023-10-07 17:35:03 +0100

    Add files via upload

---
+++ Exercice_1.js
@@ -0,0 +1,20 @@
+"use strict"
+
+let a = 4;
+let b = 4;
+let c = 3;
+
+if (a == b)
+{
+    console.log("Est égal à b");
+}
+
+else if (a == c)
+{
+    console.log("Est égal à c");
+}
+
+else 
+{
+    console.log("Est égal à rien");
+};
